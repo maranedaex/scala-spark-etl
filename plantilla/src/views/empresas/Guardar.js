81f098a872b26296a45adbf6e6b0f936f41d75ff
Jose Campos <josigna.cp@gmail.com>
2021-05-03 21:24:09 -0400

    se realizan cambios esteticos y se agrega formulario y tabla de empresas (archivos agregados)

---
+++ plantilla/src/views/empresas/Guardar.js
@@ -0,0 +1,69 @@
+import React from "react";
+
+// components
+
+export default function CardUsers() {
+  return (
+    <>
+      <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-100 border-0">
+        <div className="rounded-t bg-white mb-0 px-6 py-6">
+          <div className="text-center flex justify-between">
+            <h6 className="text-blueGray-700 text-xl font-bold">Registro de Empresas</h6>
+          </div>
+        </div>
+        <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
+          <form>
+            <h6 className="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
+              Informacion de la Empresa
+            </h6>
+            <div className="flex flex-wrap">
+              <div className="w-full lg:w-6/12 px-4">
+                <div className="relative w-full mb-3">
+                  <label
+                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                    htmlFor="">
+                    Razon Social
+                  </label>
+                  <input
+                    type="text"
+                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                    defaultValue=""/>
+                </div>
+              </div>
+
+              <div className="w-full lg:w-4/12 px-4">
+                <div className="relative w-full mb-3">
+                  <label
+                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                    htmlFor="grid-password">
+                    Usuario Encargado
+                  </label>
+                  <select className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150">
+                    <option>Seleccione</option>
+                    <option>Usuario 1</option>
+                    <option>Usuario 2</option>
+                    <option>Usuario 3</option>
+                  </select>
+                </div>
+              </div>
+          
+            </div>
+        
+
+          </form>
+        </div>
+
+        <div className="rounded-t bg-white mb-0 px-6 py-6">
+          <div className="text-center flex justify-between">
+            <button
+                className="bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
+                type="button"
+              >
+                Guardar
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
