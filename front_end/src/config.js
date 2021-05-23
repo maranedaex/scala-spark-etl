37b4cfa5dc7d91e7819519086faebd0adf5828f9
Monica Araneda <74863522+monicaaraneda@users.noreply.github.com>
2021-05-22 22:21:05 -0400

    Update config.js
    .

--- front_end/src/config.js
+++ front_end/src/config.js
@@ -1,38 +1,38 @@
 module.exports = function () {
-    console.log(`Usando config: ${process.env.REACT_APP_NODE_ENV}`)
-    switch(process.env.REACT_APP_NODE_ENV){
-      case "development":
-        return {
-          "backend": {
-            "hostname": "localhost",
-            "port": "5000",
-            "baseUrl": "http://localhost:5000"
-          }
-        };
-      case "staging":
-        return {
-          "backend": {
-            "hostname": "159.65.230.54",
-            "port": "3038",
-            "baseUrl": "http://159.65.230.54:3038"
-          }
-        };
-      case "docker":
-        return {
-          "backend": {
-            "hostname": "localhost",
-            "port": "3038",
-            "baseUrl": "http://localhost:3038"
-          }
-        };
-      default:
-        return {
-          "backend": {
-            "hostname": "localhost",
-            "port": "5000",
-            "baseUrl": "http://localhost:5000"
-          }
-        };
-    }
-  
+  console.log(`Usando config: ${process.env.REACT_APP_NODE_ENV}`)
+  switch(process.env.REACT_APP_NODE_ENV){
+    case "development":
+      return {
+        "backend": {
+          "hostname": "localhost",
+          "port": "5000",
+          "baseUrl": "http://localhost:5000"
+        }
+      };
+    case "staging":
+      return {
+        "backend": {
+          "hostname": "159.65.230.54",
+          "port": "3038",
+          "baseUrl": "http://159.65.230.54:3038"
+        }
+      };
+    case "docker":
+      return {
+        "backend": {
+          "hostname": "localhost",
+          "port": "3038",
+          "baseUrl": "http://localhost:3038"
+        }
+      };
+    default:
+      return {
+        "backend": {
+          "hostname": "localhost",
+          "port": "5000",
+          "baseUrl": "http://localhost:5000"
+        }
+      };
   }
+
+}
